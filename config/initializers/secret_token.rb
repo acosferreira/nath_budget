09385c0d8c9d898ecf36d475fed2eb8024af5c7e
acosferreira <acosferreira@gmail.com>
2013-08-13 01:40:13 +0200

    add config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bbm::Application.config.secret_token = 'c9269c0b574e6240503532743273d09a83b885c5c031b617fe5a93b116fd802310fa14bab818a78c050b83856a034c2d5bb767740d52046cb1560007c7a67869'
